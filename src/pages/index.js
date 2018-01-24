e20872734eba2e26c45a99acbd7ac79dfde42831
Louis DeScioli <louis.descioli@gmail.com>
2018-01-23 20:05:43 -0500

    Adds the octokitty to the site, adds a README

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'react-helmet';
+import Octokitty from '../Octokitty';
 import App from '../App';
 
 import './style.css';
 
-export default App;
+export default () => (
+  <div>
+    <Octokitty repo="lourd/react-google-sheets" />
+    <App />
+  </div>
+);
